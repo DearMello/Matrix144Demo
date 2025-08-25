95a52ce900fcb9dd638955a31d67b70e2a4c20d7
DearMello <maleykahuseynzade13@gmail.com>
2025-08-25 11:30:03 +0400

    Add files via upload

---
+++ 1TokenYoxlaMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace lanetMiddlewareapp
+{
+    public class _1TokenYoxlaMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public _1TokenYoxlaMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var token = context.Request.Headers["token"].ToString();
+            var tokenhisseleri = token.Split(':');
+            var herfler = tokenhisseleri[0];
+            var vaxt = int.Parse(tokenhisseleri[1]);
+
+            if(herfler.Length != 5)
+            {
+                context.Response.StatusCode = 401;
+                return;
+            }
+
+            int saat = vaxt / 100;
+            int deqiqe = vaxt % 100;
+
+            int indi = DateTime.Now.Hour * 100 + DateTime.Now.Minute;
+            if(vaxt > indi)
+            {
+                context.Response.StatusCode = 401;
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+}
---
+++ 2YeniTokenMidlleware.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace lanetMiddlewareapp
+{
+    public class _2YeniTokenMidlleware
+    {
+        private readonly RequestDelegate _next;
+        public _2YeniTokenMidlleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            context.Response.Headers["refreshToken"] = DateTime.Now.AddMinutes(5).ToString();
+            await _next(context);
+        }
+    }
+}
